ac3dc5726519c4ab32ea3bdd845c41aed52f5e4a
Diego <diegoyon@outlook.com>
2024-04-05 17:05:42 -0600

    fix links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,13 @@ import { PiShoppingCartLight } from "react-icons/pi";
 import { FaPhone } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import HighTideLogo from "../assets/high-tide-aviation-logo-white.png";
+import {
+  FACEBOOK_URL,
+  INSTAGRAM_URL,
+  PHONE_NUMBER,
+  YOUTUBE_URL,
+  EMAIL_ADDRESS,
+} from "../consts.ts";
 
 const Navbar = ({ pathname }) => {
   const [openMobile, setOpenMobile] = useState(false);
@@ -82,12 +89,20 @@ const Navbar = ({ pathname }) => {
               </div>
               <div className="hidden lg:flex items-center gap-3 text-white border-l border-white/20 pl-10">
                 <PiShoppingCartLight className="size-8 cursor-pointer hover:text-white/80 duration-300" />
-                <div className="group border p-3 border-mustard-yellow rounded-full bg-mustard-yellow hover:bg-white hover:border-white duration-300 cursor-pointer">
+                <a
+                  href={`tel:${PHONE_NUMBER}`}
+                  className="group border p-3 border-mustard-yellow rounded-full bg-mustard-yellow hover:bg-white hover:border-white duration-300"
+                >
                   <FaPhone className="size-4 group-hover:text-black duration-300" />
-                </div>
+                </a>
                 <div className="hidden 2xl:block">
                   <p className="text-sm text-white/80">Call Anytime</p>
-                  <p className="font-medium">1-800-123-4567</p>
+                  <a
+                    href={`tel:${PHONE_NUMBER}`}
+                    className="font-medium hover:text-mustard-yellow duration-300"
+                  >
+                    {PHONE_NUMBER}
+                  </a>
                 </div>
               </div>
             </div>
@@ -186,20 +201,26 @@ const Navbar = ({ pathname }) => {
 
         <div className="p-5 text-white flex flex-col gap-3 overflow-hidden w-fit">
           <div className="flex gap-3 items-center">
-            <div className="border p-2 w-fit border-mustard-yellow rounded-full bg-mustard-yellow hover:bg-white hover:border-white duration-300 cursor-pointer">
+            <a
+              href={`mailto:${EMAIL_ADDRESS}`}
+              className="border p-2 w-fit border-mustard-yellow rounded-full bg-mustard-yellow cursor-pointer"
+            >
               <MdEmail className="size-3" />
-            </div>
-            <p>info@hightideaviation.com</p>
+            </a>
+            <a href={`mailto:${EMAIL_ADDRESS}`}>{EMAIL_ADDRESS}</a>
           </div>
           <div className="flex gap-3 items-center">
-            <div className="border p-2 w-fit border-mustard-yellow rounded-full bg-mustard-yellow hover:bg-white hover:border-white duration-300 cursor-pointer">
+            <a
+              href={`tel:${PHONE_NUMBER}`}
+              className="border p-2 w-fit border-mustard-yellow rounded-full bg-mustard-yellow"
+            >
               <FaPhone className="size-3" />
-            </div>
-            <p>1-800-123-4567</p>
+            </a>
+            <a href={`tel:${PHONE_NUMBER}`}>{PHONE_NUMBER}</a>
           </div>
 
           <div className="flex gap-3 mt-2">
-            <a href="#">
+            <a href={FACEBOOK_URL} target="_blank">
               <span className="sr-only">Facebook</span>
               <svg
                 className="size-6"
@@ -214,7 +235,7 @@ const Navbar = ({ pathname }) => {
                 />
               </svg>
             </a>
-            <a href="#">
+            <a href={INSTAGRAM_URL} target="_blank">
               <span className="sr-only">Instagram</span>
               <svg
                 className="size-6"
@@ -229,33 +250,7 @@ const Navbar = ({ pathname }) => {
                 />
               </svg>
             </a>
-            <a href="#">
-              <span className="sr-only">X</span>
-              <svg
-                className="size-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
-              </svg>
-            </a>
-            <a href="#">
-              <span className="sr-only">GitHub</span>
-              <svg
-                className="size-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
-            <a href="#">
+            <a href={YOUTUBE_URL} target="_blank">
               <span className="sr-only">YouTube</span>
               <svg
                 className="size-6"
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Astro Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const PHONE_NUMBER = "(910) 477-1926";
+export const ADDRESS_LINE_1 = "4345 Airport Rd SE";
+export const ADDRESS_LINE_2 = "Southport, NC 28461";
+export const YOUTUBE_URL = "https://www.youtube.com/@hightideaviation";
+export const FACEBOOK_URL = "https://www.facebook.com/flyhightide";
+export const INSTAGRAM_URL = "https://www.instagram.com/hightideaviation/";
+export const EMAIL_ADDRESS = "info@hightideaviation.com";
