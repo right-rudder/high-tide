f955e91d23f2b1a06f8dc1c94c54d51acaf37d12
Diego <diegoyon@outlook.com>
2024-04-05 18:59:48 -0600

    add flight programs section

---
+++ src/data/flightPrograms.js
@@ -0,0 +1,30 @@
+export const flightPrograms = [
+  {
+    heading: "Private Pilot",
+    subheading: "Start Your Journey",
+    description:
+      "If you're new to aviation, we have the resources and training to help you earn your wings.",
+    imagePath: "/src/assets/flight-program-1.jpg",
+  },
+  {
+    heading: "Commercial Pilot",
+    subheading: "Earn Your Certifications",
+    description:
+      "Our flight training programs are designed to help you earn your certifications and ratings.",
+    imagePath: "/src/assets/flight-program-2.jpg",
+  },
+  {
+    heading: "Flight Instructor",
+    subheading: "Learn About Us",
+    description:
+      "Learn more about High Tide Aviation and the services we offer.",
+    imagePath: "/src/assets/flight-program-3.jpg",
+  },
+  {
+    heading: "Helicopter Training",
+    subheading: "Maintenance & Repairs",
+    description:
+      "Our team of experienced mechanics can help keep your aircraft in top condition.",
+    imagePath: "/src/assets/flight-program-4.jpg",
+  },
+];
