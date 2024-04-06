0a5f7cb602323978912e3fd068d1a357b0c1b3bf
Diego <diegoyon@outlook.com>
2024-04-06 13:04:59 -0600

    add reviews

---
+++ src/data/reviews.js
@@ -0,0 +1,12 @@
+export const reviews = [
+  {
+    name: "John Doe",
+    review:
+      "High Tide Aviation is the best flight school I've ever attended. The instructors are top-notch and the facilities are first class.",
+  },
+  {
+    name: "Jane Smith",
+    review:
+      "I had a great experience learning to fly at High Tide Aviation. The instructors were knowledgeable and the aircraft were well-maintained.",
+  },
+];
