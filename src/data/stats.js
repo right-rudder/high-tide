0f244c1b94b2f540ec66b1e17b8b170dc08b4fcf
Diego <diegoyon@outlook.com>
2024-04-06 12:37:44 -0600

    add stats

---
+++ src/data/stats.js
@@ -0,0 +1,22 @@
+export const stats = [
+  {
+    icon: "PiStudent",
+    value: 1000,
+    title: "Students graduated",
+  },
+  {
+    icon: "SiSpeedtest",
+    value: 2000,
+    title: "Checkride pass rate",
+  },
+  {
+    icon: "CiClock1",
+    value: 3000,
+    title: "Combined flight hours",
+  },
+  {
+    icon: "BsAirplaneEngines",
+    value: 14,
+    title: "Unique aircraft to choose from",
+  },
+];
