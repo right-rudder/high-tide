a870883a46d3e8a2560ab259bf7067f7485d5d82
Diego <diegoyon@outlook.com>
2024-04-06 17:57:48 -0600

    add blog posts

---
+++ src/data/latestPosts.js
@@ -0,0 +1,17 @@
+export const latestPosts = [
+  {
+    title: "How to Choose a Flight School asdfasdfsf sfasdf asdf asdf as fasfs",
+    date: "15 Sept, 2024",
+    imagePath: "/src/assets/post-1.jpg",
+  },
+  {
+    title: "What to Expect on Your First Flight Lesson",
+    date: "10 Sept, 2024",
+    imagePath: "/src/assets/post-2.jpg",
+  },
+  {
+    title: "How to Become a Pilot",
+    date: "5 Sept, 2024",
+    imagePath: "/src/assets/post-3.jpg",
+  },
+];
