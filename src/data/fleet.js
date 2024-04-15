eceec8ed8b30af898fea77af288dc396aa91d384
Diego <diegoyon@outlook.com>
2024-04-15 15:14:45 -0600

    add pages for each plane detail

--- src/data/fleet.js
+++ src/data/fleet.js
@@ -1,36 +1,42 @@
 export const fleet = [
   {
     name: "n7085g",
+    slug: "n7085g",
     rentalPrice: "$150 hr/wet",
     year: 1978,
     imagePath: "/src/assets/n7085g.jpg",
   },
   {
     name: "n7994x",
+    slug: "n7994x",
     rentalPrice: "$150 hr/wet",
     year: 1978,
     imagePath: "/src/assets/n7994x.jpg",
   },
   {
     name: "n8409l",
+    slug: "n8409l",
     rentalPrice: "$150 hr/wet",
     year: 1978,
     imagePath: "/src/assets/n8409l.jpg",
   },
   {
     name: "n79136",
+    slug: "n79136",
     rentalPrice: "$150 hr/wet",
     year: 1978,
     imagePath: "/src/assets/n79136.jpg",
   },
   {
     name: "n79475",
+    slug: "n79475",
     rentalPrice: "$150 hr/wet",
     year: 1978,
     imagePath: "/src/assets/n79475.jpg",
   },
   {
     name: "Bonanza",
+    slug: "n4457s",
     rentalPrice: "$150 hr/wet",
     year: 1978,
     imagePath: "/src/assets/n4457s.jpg",
