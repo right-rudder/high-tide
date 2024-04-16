bbcd9fc8aa127f53cdd13072467b47691bc13606
Diego <diegoyon@outlook.com>
2024-04-16 14:36:02 -0600

    add financing pages and fix footer

---
+++ src/data/financialOptions.js
@@ -0,0 +1,32 @@
+export const financialOptions = [
+  {
+    name: "Meritize",
+    imagePath: "/src/assets/meritize-logo.png",
+    description:
+      "High Tide Aviation has partnered with the nation’s leading educational funding company to help prospective pilots achieve their aviation goals and get a free flight training quote.",
+  },
+  {
+    name: "Meratas",
+    imagePath: "/src/assets/meratas-logo.png",
+    description:
+      "High Tide Aviation has also partnered with Meratas to help prospective pilots achieve their aviation goals. The Meratas Lender Marketplace gives you personalized loan offers with multiple lending partners. Get instantaneous pre-qualifications to finance your education and general living expenses. No risk or impact on your credit score. All information, secured.",
+  },
+  {
+    name: "Federal Aviation Administration",
+    imagePath: "/src/assets/faa-logo.svg",
+    description:
+      "Free money for flight training is everywhere, just have to know where to look.",
+  },
+  {
+    name: "Aircraft Owners and Pilots Association",
+    imagePath: "/src/assets/aopa.jpg",
+    description:
+      "Membership has it’s privileges for flight training financing with AOPA.",
+  },
+  {
+    name: "Experimental Aircraft Association",
+    imagePath: "/src/assets/eaa-logo.png",
+    description:
+      "EAA’s scholarships are supported by dozens of donors, benefactors, and others who “pass it forward” by endowing these programs. In all, EAA awards more than $1 million in aviation scholarship money via various programs each year.",
+  },
+];
