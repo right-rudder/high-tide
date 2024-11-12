a2a9a91977f3388f451c1fbb5cd90750c4eb9f20
Diego <diegoyon@outlook.com>
2024-11-12 13:36:57 -0600

    swap airplane

--- src/data/fleet.js
+++ src/data/fleet.js
@@ -22,11 +22,11 @@ export const fleet = [
     ],
   },
   {
-    name: "N7994X",
-    slug: "N7994X",
+    name: "N35461",
+    slug: "N35461",
     rentalPrice: "$190 /hr wet",
     usableFuel: "38 gal",
-    model: "Cessna 172B",
+    model: "Cessna 172I",
     equipment: [
       "Dual G5",
       "PS PAR 200A Audiopanel/Comm",
@@ -34,8 +34,8 @@ export const fleet = [
       "Garmin GTX 330 Transponder",
       "JPI Fuel Flow Gauge",
     ],
-    imagePath: "/src/assets/N7994X.jpg",
-    imagesArray: ["/src/assets/N7994X.jpg", "/src/assets/N7994X-panel.webp"],
+    imagePath: "/src/assets/N35461.jpg",
+    imagesArray: ["/src/assets/N35461.jpg"],
     description: [
       "The Cessna 172 is the gold standard for new pilots. Its forgiving flight characteristics and robust reliability make it the perfect choice for initial flight training. The high-wing design offers exceptional stability and visibility, boosting student confidence as they master essential flying skills. The aircraft responds gently to control inputs and recovers smoothly from errors, instilling the poise needed to become a competent pilot. With widespread availability and easy access to maintenance expertise, the Cessna 172 keeps operational costs low, allowing for consistent training schedules.",
       "Beyond its physical attributes, the aircraft is equipped with modern avionics and instrumentation, giving students a real-world flying experience. Its versatility extends from solo flights to instrument training, making the Cessna 172 a fundamental tool in developing proficient pilots",
@@ -82,7 +82,7 @@ export const fleet = [
     slug: "N8409L",
     rentalPrice: "$190 /hr wet",
     usableFuel: "38 gal",
-    model: "Cessna 172K",
+    model: "Cessna 172I",
     equipment: [
       "GNX™ 175 (Garmin) w/ WAAS",
       "Garmin G5",
