4644d6b12e6905641c2973652ea7d45af9f4a9f5
Diego Yon <diegoyon@outlook.com>
2025-03-31 22:00:38 -0500

    add visit school

---
+++ src/data/locations.js
@@ -0,0 +1,18 @@
+export const locations = [
+  {
+    name: "Southport School Tour",
+    location: "Southport, NC",
+    price: "FREE",
+    imagePath:
+      "/src/assets/cfi-and-student-at-high-tide-aviation-southport-nc.jpg",
+    url: "https://www.flightcircle.com/shop/5c9443ea346a/4000020399",
+  },
+  {
+    name: "St Simons School Tour",
+    location: "St Simons Island, GA",
+    price: "FREE",
+    imagePath:
+      "/src/assets/private-pilot-in-front-of-cessna-172-skyhawk-st-simons-island-ga.jpg",
+    url: "https://www.flightcircle.com/shop/5c9443ea346a/4000020401",
+  },
+];
