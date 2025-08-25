01da36a4a4479b15c6634f0a18a47b3309c7541c
Lucas Rodrigues <lcrodrigues1995@gmail.com>
2025-08-25 14:39:47 -0300

    Update locations.js

--- src/data/locations.js
+++ src/data/locations.js
@@ -15,4 +15,12 @@ export const locations = [
       "/src/assets/private-pilot-in-front-of-cessna-172-skyhawk-st-simons-island-ga.jpg",
     url: "https://www.flightcircle.com/shop/5c9443ea346a/4000020401",
   },
+  {
+    name: "Wilmington School Tour",
+    location: "Wilmington, NC",
+    price: "FREE",
+    imagePath:
+      "/src/assets/wilmington-nc-cfi-checking-aircraft.jpg",
+    url: "https://www.flightcircle.com/shop/5c9443ea346a/4000026078",
+  },
 ];
